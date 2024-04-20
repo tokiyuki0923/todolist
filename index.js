9e339ef1a61be2875c9482503d9ad1b1914d5b9f
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-04-21 00:00:34 +0900

    Enterを押された時にliタグを追加するコード（まだ途中）

--- index.js
+++ index.js
@@ -2,11 +2,25 @@
 
 //formタグとはgetFormであると指定
 //inputタグはgetInputであると指定
-const getForm = document.getElementById("formtagu");
+const getForm = document.querySelector("formtagu");
 const getInput = document.getElementById("new")
 
 // フォームタグがEnterされた時、普通なら再レンダリングが発生してしまうが、それを止める。preventDefaultは、本来なら起こるブラウザの挙動を止めるためのメソッド（WebAPI）
 getForm.addEventListener("submit",function (event){
     event.preventDefault();
     console.log(getInput.value);
+    add();
 });
+
+// Enter押された時に、liタグを追加する。
+function add (){
+
+    // Enter押された時に、liタグを追加する。
+    const li = document.createElement("li")
+
+    // そのliのテキストはinputタグに書かれている内容とする
+    li.innerText = getInput.value
+
+    // 実際に追加されたliタグにclassを付与する
+    li.classList.add("list-group-item")
+}
