b73eb9cd2e159c55078a6421316424384819e7b3
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-04-20 23:31:20 +0900

    フォームタグがEnterされた時、普通なら再レンダリングが発生してしまうがそれを止める。preventDefaultは本来なら起こるブラウザの挙動を止めるためのメソッド（WebAPI）

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 "use strict";
+
+//formタグとはgetFormであると指定
+//inputタグはgetInputであると指定
+const getForm = document.getElementById("formtagu");
+const getInput = document.getElementById("new")
+
+// フォームタグがEnterされた時、普通なら再レンダリングが発生してしまうが、それを止める。preventDefaultは、本来なら起こるブラウザの挙動を止めるためのメソッド（WebAPI）
+getForm.addEventListener("submit",function (event){
+    event.preventDefault();
+    console.log(getInput.value);
+});
