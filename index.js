54a4ac33e0e25762b2a2497bb7634e37d3a42a26
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-04-21 23:21:04 +0900

    だめだ、11~18行目のコードがあるのにリロードするとローカルストレージにあるはずのタスクが全て消えてしまう。原因がわからない。

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const todos = JSON.parse(localStorage.getItem("todos"));
 // もしローカルストレージに保存されているtodosが空じゃなかったらそれぞれを追加しなおす作業
 if(todos){
     todos.forEach(todo => {
-        add(todo)
-    });
+        add(todo);
+    })
 }
 
 //変数名.addEventListner→変数名の中身がsubmit(他にもいろんなイベントがある)が行われた時、functionの中身が行われる
@@ -28,17 +28,15 @@ getForm.addEventListener("submit", function (event){
 
 // Enter押された時に、liタグを追加すると言う関数を定義
 function add (todo){
-    let todoText = getInput.value 
-    
-    //もし、過去に保存されていたtodoがあった場合、todoにはtodoTextをいれるという処理　
-    if (todo) {
-        todoText = todo;
+
+    let todoText = getInput.value;
+
+    if(todo){
+        todoText = todo.text;
     }
 
-    // もしtodoTextの長さが1文字以上だった場合、if文の中身を実行する
-    if(todoText.length > 0){
-    
-    // Enter押された時に、liタグを追加する。
+    if(todoText){
+        //Enter押された時に、liタグを追加する。
     const li = document.createElement("li");
 
     // そのliのテキストはinputタグに書かれている内容とする
@@ -47,16 +45,44 @@ function add (todo){
     // 実際に追加されたliタグにclassを付与する
     li.classList.add("list-group-item");
 
+    if(todo && todo.completed){
+        li.classList.add("text-decoration-line-through")
+    }
+
+
+
+    // 右クリックされた時にタスクを削除するという機能
+    li.addEventListener("contextmenu",function(event){
+        event.preventDefault();
+        
+        // liを取り除く
+        li.remove();
+
+        // 取り除いた後ローカルストレージに保存
+        saveDate();
+    });
+
+
+    // タスクをクリックした時、取消線をつけるようにする
+    li.addEventListener("click",function(){
+        // classList.toggle("")で("")内に入っているクラスを付けたり消したりする
+        li.classList.toggle("text-decoration-line-through"); 
+        saveDate();
+    });
+
+
+
+
+
+
     // ulタグの中にliタグを追加させる
     ul.appendChild(li);
 
     // 最後にinputの中身を空にする
     // getInput.valueではなくてtodoTextにしたら動かなかった、なんでだろう
     getInput.value = "";
-
     // 下にあるsaveDateという関数を発動
     saveDate(); 
-
     }
 }
 // saveDateという関数を定義、最終的にはliタグを全て配列の形にしてローカルストレージに保存する
@@ -66,14 +92,13 @@ function saveDate(){
     const lists = document.querySelectorAll("li");
 
     // 空っぽの配列をtodosという変数に定義する
-    const todos = [];
-
-    // liタグのインナーテキストをそれぞれ全て取得する
-    lists.forEach(list =>{
-        // liタグのinnerTextを先ほど定義したtodosという配列にpushする
+    let todos = [];
+    
+    // タスクのインナーテキストとクラス名"text-decoration-line-through"が追加されているかどうかをそれぞれ全て取得する
+    lists.forEach(list => {
         todos.push(list.innerText);
     });
-
+    
     // localStrage.setItemでローカルストレージに保存する。保存したいデータが複数（配列）だった場合、JSON形式の文字列で保存しなくちゃいけない。その時使うのがJSON.stringify()となる
     localStorage.setItem("todos",JSON.stringify(todos));
 }
