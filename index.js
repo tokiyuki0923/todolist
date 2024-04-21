bcf5553f6d6c810d801d61f9e185e29ef2464cf1
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-04-21 16:00:36 +0900

    Enter押された時に、liを追加するようにした。liのinnerTextはinputタグのvalue

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
-"use strict";
 
-//formタグとはgetFormであると指定
+
+//formタグはgetFormであると指定
 //inputタグはgetInputであると指定
-const getForm = document.querySelector("formtagu");
-const getInput = document.getElementById("new")
+// ulタグはulであると指定
+const getForm = document.getElementById("formtagu");
+const getInput = document.getElementById("new");
+const ul = document.getElementById("ul");
 
 // フォームタグがEnterされた時、普通なら再レンダリングが発生してしまうが、それを止める。preventDefaultは、本来なら起こるブラウザの挙動を止めるためのメソッド（WebAPI）
-getForm.addEventListener("submit",function (event){
+//変数名.addEventListner→変数名の中身がsubmit(他にもいろんなイベントがある)が行われた時、functionの中身が行われる
+getForm.addEventListener("submit", function (event){
     event.preventDefault();
-    console.log(getInput.value);
+    
+    // 下に書いてあるadd関数を実行
     add();
 });
 
-// Enter押された時に、liタグを追加する。
+// Enter押された時に、liタグを追加すると言う関数を定義
 function add (){
-
-    // Enter押された時に、liタグを追加する。
-    const li = document.createElement("li")
+    let todoText = getInput.value 
+    if(todoText.length > 0){
+            // Enter押された時に、liタグを追加する。
+    const li = document.createElement("li");
 
     // そのliのテキストはinputタグに書かれている内容とする
-    li.innerText = getInput.value
+    li.innerText = todoText;
 
     // 実際に追加されたliタグにclassを付与する
-    li.classList.add("list-group-item")
+    li.classList.add("list-group-item");
+
+    // ulタグの中にliタグを追加させる
+    ul.appendChild(li);
+
+    // 最後にinputの中身を空にする
+    // getInput.valueではなくてtodoTextにしたら動かなかった、なんでだろう
+    getInput.value = "";
+    }
 }
+
